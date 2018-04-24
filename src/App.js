139172440f3a60397a9d41c52e1b9434a4aecfba
Emma Walker <emmmarosewalker@gmail.com>
2018-04-24 23:39:32 +1000

    better commenting and refactored if else statements to ternary operators in getApi func

--- src/App.js
+++ src/App.js
@@ -31,21 +31,18 @@ class App extends Component {
     }
   }
   getApi(val, isNum){
-    let params = {};
+    /* Uses val (the input the user selected, either num. paragraphs or return type) and the isNum boolean (true if number
+    of paragraphs was changed) to set parameters to make a get request to the Bacon Ipsum API using axios. The response sets the state of the
+    text, after which React automatically re-renders the output with the changed state. */
+
+    let params = {
+      type : 'meat-and-filler' // from Bacon Ispum API. Docs : all-meat for meat only or meat-and-filler for meat mixed with miscellaneous ‘lorem ipsum’ filler.
+    };
+
     // Check if the value was from changing no. paragraphs or html/plain text. Set API params accordingly
-    if (!isNum){
-      params = {
-        type: 'meat-and-filler',
-        format: val,
-        paras: this.state.numParagraphs
-      }
-    } else {
-      params = {
-        type: 'meat-and-filler',
-        format: this.state.returnType,
-        paras: val
-      }
-    }
+    isNum ? params['paras'] = val : params['paras'] = this.state.numParagraphs;
+    isNum ? params['format'] = this.state.returnType : params['format'] = val;
+
     // API call
     const apiUrl = "https://baconipsum.com/api";
     axios.get(apiUrl, {
@@ -66,7 +63,7 @@ class App extends Component {
       <h1>Bacon Ipsum Generator</h1>
       <Input onChange={this.handleChange} />
       <Card cardContent={this.state.text} />
-      <p>This generator is based on the <a href="https://baconipsum.com/json-api/">Bacon Ipsum API</a></p>
+      <p>This generator is based on the <a href="https://baconipsum.com/json-api/">Bacon Ipsum API</a></p> // Credit to original API author
       </div>
       );
   }
